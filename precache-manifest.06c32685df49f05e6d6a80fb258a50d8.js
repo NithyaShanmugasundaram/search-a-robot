c67fb55e3f7a839dc541ffafcf672a9b268fa202
Ganesh kumar M <gunz@Ganeshs-MacBook-Pro.local>
2019-06-19 13:18:24 +0800

    Updates

---
+++ precache-manifest.06c32685df49f05e6d6a80fb258a50d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7363c74075c0ed5af4f29eda2b2b3fb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d2c8c5924c12513f8ae",
+    "url": "/static/css/2.9878de88.chunk.css"
+  },
+  {
+    "revision": "b29cacdc21df210311d4",
+    "url": "/static/css/main.b4702168.chunk.css"
+  },
+  {
+    "revision": "7d2c8c5924c12513f8ae",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "b29cacdc21df210311d4",
+    "url": "/static/js/main.47ecea2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
